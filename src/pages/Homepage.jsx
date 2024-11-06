658c40f4f95bca8ae5d9681ff67590c5cddad407
EllaLiu0401 <ellatong0515@gmail.com>
2024-11-06 20:59:29 +1100

    feat: add Navlink for each page.

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom";
+import PageNav from "../components/PageNav";
 function Homepage() {
-  return <div>WolrdWise</div>;
+  return (
+    <div>
+      <PageNav />
+      <h1>WolrdWise</h1>
+      <Link to="pricing">Pricing</Link>
+    </div>
+  );
 }
 
 export default Homepage;
